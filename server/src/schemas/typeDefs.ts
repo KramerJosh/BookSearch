dad3576d21d38ffdf17acccccdb8b45907a39933
Josh Kramer <kramer@nyu.edu>
2025-03-01 13:23:13 -0500

    starting typeDefs and Resolvers

---
+++ server/src/schemas/typeDefs.ts
@@ -0,0 +1,30 @@
+const typeDefs = `
+type Book {
+    _id: ID!
+    bookId: String!
+    title: String!
+    authors: String[]
+    description: String!
+    image: String
+    link: String
+}
+
+type User {
+    _id: ID!
+    username: String!
+    email: String!
+    password: String!
+    saveBooks: BookDocument[] ##Start HERE
+}
+
+type Query {
+
+}
+
+type Mutation {
+
+}
+
+`;
+
+export default typeDefs;
